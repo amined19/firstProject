7fdfff3930b1191c88405aa292e2c7976e560b83
amined19 <amined.19@gmail.com>
2016-10-02 21:52:52 +0200

    4eme commit

--- src/firstProject/Main.java
+++ src/firstProject/Main.java
@@ -2,7 +2,7 @@ package firstProject;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // test du conflit depuis le site
 
 		// a comment
 		System.out.println("first project test");
